7a7dce52c32a4ef421e90392c943b346955076a8
Heddad Amine <Heddad@fer-ge.ch>
2019-07-15 16:07:25 +0200

    Redux and Thunk added

--- ppmtool-react-client/src/App.js
+++ ppmtool-react-client/src/App.js
@@ -4,13 +4,21 @@ import './App.css';
 import Dashboard from './components/Dashboard';
 import Header from './layout/Header';
 import 'bootstrap/dist/css/bootstrap.min.css';
-
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+import AddProject from './components/project/AddProject';
+import { Provider } from 'react-redux';
+import store from './store';
 function App() {
 	return (
-		<div className="App">
-			<Header />
-			<Dashboard />
-		</div>
+		<Provider store={store}>
+			<Router>
+				<div className="App">
+					<Header />
+					<Route exact path="/dashboard" component={Dashboard} />
+					<Route exact path="/addProject" component={AddProject} />
+				</div>
+			</Router>
+		</Provider>
 	);
 }
 
